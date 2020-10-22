316851b9aa1cae0d0ed05e28eb71558efc7a3b86
Otoniel Andrade <otonielandrade14@gmail.com>
2020-10-21 20:35:50 -0600

    tailwind.config.js colors and fonts personalized

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      rale: ["Roboto Mono"],
+    },
+    extend: {
+      colors: {
+        danger: "#fc5185",
+        buenardo: "#3fc1c9",
+        infor: {
+          100: "#53354a",
+          200: "#903749",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
